1f3247b1cce86b1104be7f2f89b324f423a5c208
poulp <mathieu.nerv@gmail.com>
2015-02-16 15:13:19 +0100

    create team limited permissions to apptracker

--- apptracker/forms.py
+++ apptracker/forms.py
@@ -1,7 +1,9 @@
 
 from django import forms
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.models import Permission
 
-from apptracker.models import Issue, Label, Comment
+from apptracker.models import Issue, Label, Comment, TrackerTeam, Project
 
 
 class NewIssueForm(forms.ModelForm):
@@ -34,3 +36,18 @@ class CommentForm(forms.ModelForm):
     class Meta(object):
         model = Comment
         fields = ['text']
+
+
+class TrackerTeamForm(forms.ModelForm):
+    class Meta(object):
+        model = TrackerTeam
+        fields = ['name', 'color', 'permissions']
+        widgets = {
+            'permissions': forms.CheckboxSelectMultiple()
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(TrackerTeamForm, self).__init__(*args, **kwargs)
+        apptracker_content_types = ContentType.objects.get_for_models(
+            Issue, Label, Comment, TrackerTeam, Project).values()
+        self.fields['permissions'].queryset = Permission.objects.filter(content_type__in=apptracker_content_types)
--- apptracker/views/teams.py
+++ apptracker/views/teams.py
@@ -4,6 +4,7 @@ from django.core.urlresolvers import reverse
 
 from apptracker.models import TrackerTeam
 from apptracker.mixins import LoginRequiredMixin, ProjectMixin
+from apptracker.forms import TrackerTeamForm
 
 
 class TeamListView(LoginRequiredMixin, ProjectMixin, ListView):
@@ -17,6 +18,7 @@ class TeamListView(LoginRequiredMixin, ProjectMixin, ListView):
 class TeamCreateView(LoginRequiredMixin, ProjectMixin, CreateView):
     template_name = 'apptracker/projects/teams/create.html'
     model = TrackerTeam
+    form_class = TrackerTeamForm
 
     def get_success_url(self):
         return reverse('team-list', kwargs={'project_pk': self.kwargs['project_pk']})
