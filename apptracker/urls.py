d2aa1376441da1b7985cbf36c176f5f6a9743d7c
poulp <mathieu.nerv@gmail.com>
2015-02-19 10:29:44 +0100

    remove unusued views

--- apptracker/urls.py
+++ apptracker/urls.py
@@ -10,7 +10,6 @@ project_urls = [
     # Projects
     url(r'^$', projects.ProjectListView.as_view(), name='project-list'),
     url(r'^create/$', projects.ProjectCreateView.as_view(), name='project-create'),
-    url(r'^(?P<project_pk>[0-9]+)/$', projects.ProjectDetailView.as_view(), name='project-detail'),
     url(r'^(?P<project_pk>[0-9]+)/edit$', projects.ProjectUpdateView.as_view(), name='project-edit'),
     url(r'^(?P<project_pk>[0-9]+)/delete$', projects.ProjectDeleteView.as_view(), name='project-delete'),
 
@@ -31,12 +30,10 @@ project_urls = [
     url(r'^(?P<project_pk>[0-9]+)/issues/(?P<issue_pk>[0-9]+)/edit$', issues.IssueEditView.as_view(), name='issue-edit'),
     url(r'^(?P<project_pk>[0-9]+)/issues/(?P<issue_pk>[0-9]+)/close', issues.IssueCloseView.as_view(), name='issue-close'),
     url(r'^(?P<project_pk>[0-9]+)/issues/(?P<issue_pk>[0-9]+)/comment', issues.IssueCommentView.as_view(), name='issue-comment'),
-
     url(r'^(?P<project_pk>[0-9]+)/issue/new/$', issues.IssueCreateView.as_view(), name='issue-new'),
     url(r'^(?P<project_pk>[0-9]+)/issues/(?P<issue_pk>[0-9]+)/delete$', issues.IssueDeleteView.as_view(), name='issue-delete'),
-    ]
+]
 
 urlpatterns = [
     url(r'^projects/', include(project_urls)),
-    url(r'^$', 'apptracker.views.general.home', name='apptracker-home')
 ]
--- apptracker/views/general.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-from django.shortcuts import render
-
-
-def home(request):
-    return render(request, "apptracker/general/index.html", {})
